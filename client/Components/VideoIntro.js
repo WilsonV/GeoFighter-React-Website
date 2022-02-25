de560e045c799ab7b1af03c8947d10b23985ce44
Wilson Voltaire <wilsonvoltaire@gmail.com>
2022-02-25 14:18:35 -0500

    added register modal, not done yet

--- client/Components/VideoIntro.js
+++ client/Components/VideoIntro.js
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BackgroundVideo from './BackgroundVideo'
+import RegisterModal from './RegisterModal'
 
 const VideoIntro = () => {
+  const [showRegisterModal, setRegisterModalVisibility] = useState(false)
+
+  function handleRegisterModal () {
+    setRegisterModalVisibility(!showRegisterModal)
+  }
+
   return (
     <div className="section">
       <BackgroundVideo />
       <img className='center-logo' src="logo_big.png" />
       <div>
         <button>Download</button>
-        <button>Register</button>
+        <button onClick={handleRegisterModal}>Register</button>
       </div>
       <h1>
         Download and Play For Free!
       </h1>
+      {showRegisterModal ? <RegisterModal handleVisibility={handleRegisterModal}/> : false}
     </div>
   )
 }
